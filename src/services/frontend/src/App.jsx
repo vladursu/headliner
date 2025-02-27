f925df512d365980aae48ee2908d7aab2ac0d1d3
Vlad Ursu <6401806+vladursu@users.noreply.github.com>
2025-02-27 21:57:06 +0000

    Mostly finish with FE

--- src/services/frontend/src/App.jsx
+++ src/services/frontend/src/App.jsx
@@ -1,12 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
+import { getHeadlines } from "@/api";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [headlinerResponse, setHeadlinerResponse] = useState("");
+  const [userQuery, setUserQuery] = useState("");
+
+  function handleQueryChange(event) {
+    setUserQuery(event.target.value);
+  }
+
+  function onClickSend() {
+    setIsLoading(true);
+    response = getHeadlines(userQuery);
+    setHeadlinerResponse(response);
+    setIsLoading(false);
+  }
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-sky-300 h-screen">
       <div className="flex flex-col items-center m-5 w-1/2">
-        <h1 className="text-xl">Headliner</h1>
-        <p>The first place to find the news that matters.</p>
+        <h1 className="font-sans text-5xl font-semibold">Headliner</h1>
+        <p className="italic text-lg">
+          The first place to find the news that matters.
+        </p>
+        <div className="flex flex-row gap-2 w-2/3 my-5">
+          <Input
+            className="bg-slate-200"
+            placeholder="What's new in tech in the London area?"
+            onChange={handleQueryChange}
+            value={userQuery}
+          />
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={onClickSend}
+            disabled={isLoading}
+          >
+            Send
+          </Button>
+        </div>
+        <div className="w-2/3">
+          {isLoading ? (
+            <Skeleton className="h-40" />
+          ) : (
+            <div className="bg-sky-300/75 p-5">{headlinerResponse}</div>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/frontend/vite.config.js
+++ src/services/frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080
-  }
-})
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
